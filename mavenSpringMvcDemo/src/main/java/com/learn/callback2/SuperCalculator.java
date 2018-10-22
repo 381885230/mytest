771806ba7fd60a2762ca4bc631e568866b61be83
yuan <yuanjin381885230@163.com>
2018-10-22 22:24:08 +0800

    callback

---
+++ mavenSpringMvcDemo/src/main/java/com/learn/callback2/SuperCalculator.java
@@ -0,0 +1,8 @@
+package com.learn.callback2;
+
+public class SuperCalculator {
+	public void add(int a, int b, doJob customer) {
+		int result = a + b;
+		customer.fillBlank(a, b, result);
+	}
+}
---
+++ mavenSpringMvcDemo/src/main/java/com/learn/callback2/doJob.java
@@ -0,0 +1,5 @@
+package com.learn.callback2;
+
+public interface doJob {
+	public void fillBlank(int a, int b, int result);
+}
