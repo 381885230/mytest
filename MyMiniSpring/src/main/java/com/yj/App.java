faeb33fb46d8ecb5ac0b608a2d63c9e315b5d72e
yuan <yuanjin381885230@163.com>
2018-11-01 01:20:16 +0800

    myspring

---
+++ MyMiniSpring/src/main/java/com/yj/App.java
@@ -0,0 +1,11 @@
+package com.yj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ MyMiniSpring/src/main/java/com/yj/demo/service/IDemoService.java
@@ -0,0 +1,9 @@
+package com.yj.demo.service;
+
+public interface IDemoService {
+
+	String getMyName(String name);
+
+	int add(Integer a, Integer b);
+
+}
